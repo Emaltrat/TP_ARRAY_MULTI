658e40de5debbc61eb4f25150a5165de556001a8
4angy <angyzalazar18@gmail.com>
2024-09-25 22:08:34 -0300

    agregué el ejercicio 2

--- TP_ARRAY_MULTI.java
+++ TP_ARRAY_MULTI.java
@@ -12,7 +12,7 @@ public class TP_ARRAY_MULTI {
         do{
             System.out.println("Ingrese la opcion:");
             System.out.println("1)Ejercicio 1 Países.\n"+
-                    "2)Ejercicio 2 \n"+
+                    "2)Ejercicio 2 Matrices. \n"+
                     "3)Ejercicio 3 \n"+
                     "4)Ejercicio 4 \n"+
                     "5)Ejercicio 5 Máquina expendedora\n" +
@@ -20,14 +20,14 @@ public class TP_ARRAY_MULTI {
             opcion = scanner.nextInt();
             scanner.nextLine();
 
-          switch (opcion){
+            switch (opcion){
                 case 1:
-                  Ej1.ejercicio1(scanner);
-                  break;
-              case 2:
-                  Ej2.EJ2();
-                  break;
-              case 3:
+                    Ej1.ejercicio1(scanner);
+                    break;
+                case 2:
+                    Ej2.MatricesYSuma();
+                    break;
+                case 3:
                   Ej3.EJ3();
                   break;
               case 4:
